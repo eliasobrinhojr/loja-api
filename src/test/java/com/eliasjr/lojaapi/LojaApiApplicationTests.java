303f6b7bc78f36ad02c3850156cf4a886a6b157a
EliasJnnr <elias.junior@innyx.com>
2024-05-13 21:16:27 -0300

    initial structure

---
+++ src/test/java/com/eliasjr/lojaapi/LojaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliasjr.lojaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
